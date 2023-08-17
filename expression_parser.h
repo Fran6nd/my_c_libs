133ea2e26645be8bdd930c4f8825a2766363acc2
Francois NOUAILLE-DEGORCE <my@secret.email>
2023-08-17 22:56:48 +0200

    wip

--- expression_parser.h
+++ expression_parser.h
@@ -1,3 +1,6 @@
+#ifndef EXPRESSION_EVALUATOR_H
+#define EXPRESSION_EVALUATOR_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,12 +15,12 @@ typedef struct {
 } Stack;
 
 // Initialize the stack
-void stackInit(Stack *s) {
+static void stackInit(Stack *s) {
     s->top = -1;
 }
 
 // Push an item onto the stack
-void stackPush(Stack *s, char val) {
+static void stackPush(Stack *s, char val) {
     if (s->top < STACK_SIZE - 1) {
         s->top++;
         s->data[s->top] = val;
@@ -28,7 +31,7 @@ void stackPush(Stack *s, char val) {
 }
 
 // Pop an item from the stack
-char stackPop(Stack *s) {
+static char stackPop(Stack *s) {
     if (s->top >= 0) {
         // Retrieve the value at the top of the stack
         char poppedValue = s->data[s->top];
@@ -46,12 +49,12 @@ char stackPop(Stack *s) {
 }
 
 // Check if a character is an operator
-int isOperator(char c) {
+static int isOperator(char c) {
     return (c == '+' || c == '-' || c == '*' || c == '/');
 }
 
 // Get the precedence of an operator
-int precedence(char c) {
+static int precedence(char c) {
     if (c == '+' || c == '-') {
         return 1;
     } else if (c == '*' || c == '/') {
@@ -132,11 +135,4 @@ double evaluateExpression(const char *expression) {
     return stackPop(&operandStack);
 }
 
-// Example:
-//int main() {
-//    const char *expression = "(2+1)*2/3-1";
-//    double result = evaluateExpression(expression);
-//    printf("Result: %lf\n", result);
-
-//    return 0;
-//}
+#endif
